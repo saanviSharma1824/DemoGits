4655ba23d0dca37f88bf5651c54d0c553e37102d
emitech990 <emitech990@emitech990s.local>
2019-03-06 16:11:56 +0530

    test commit

--- DemoGits/ViewController.swift
+++ DemoGits/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello")
         print("First User Print Hi")
+        print("Test Git")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
